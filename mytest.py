7ab8339fad7a8a520a5e008eb8ddce589e411613
ar.gilaki <ar.gilaki@gmail.com>
2019-12-23 15:16:56 +0330

    permited visitor ip

--- mytest.py
+++ mytest.py
@@ -1,15 +1,20 @@
 # ---- Flask Hello World ---- #
 # import the Flask class from the flask package
 from flask import Flask
+from flask import abort, request
 # create the application object
 app = Flask(__name__)
 # use the decorator pattern to
 # link the view function to a url
+@app.before_request
+def limit_remote_addr():
+    if request.remote_addr not in ['172.16.102.117','127.0.0.1']:
+        abort(403)  # Forbidden
 @app.route("/")
 @app.route("/hello")
 # define the view using a function, which returns a string
 def hello_world():
-return "Hello, World!"
+    return "hello   world"
 # start the development server using the run() method
 if __name__ == "__main__":
-app.run()
+    app.run(host='0.0.0.0')
